0a3fbad844cb253e5cc34afb953045ae5cc3dfc0
Paquitosoft <paquitosoftware@gmail.com>
2017-12-26 21:33:02 +0100

    Progress in add-linky component

--- src/components/home-page/add-linky/add-linky.js
+++ src/components/home-page/add-linky/add-linky.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
+import './add-linky.css';
+
 class AddLinky extends React.Component {
+
 	constructor() {
 		super();
 
 		this.state = {
-			showButton: false
+			isFolded: true
 		};
+	}
+
+	documentClickHandler = e => {
+		const isChild = [...document.querySelectorAll('.add-linky-container *')].includes(e.target);
+		if (!isChild) {
+			this.setState({ isFolded: true });
+		}
+	}
+
+	documentKeydownHandler = e => {
+		if (e.keyCode === 27) {
+			this.setState({ isFolded: true });
+		}
+	}
+
+	componentDidMount() {
+		document.addEventListener('click', this.documentClickHandler);
+		document.addEventListener('keydown', this.documentKeydownHandler);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('click', this.documentClickHandler);
+		document.removeEventListener('keydown', this.documentKeydownHandler);
 	}
 
 	render() {
@@ -18,12 +44,22 @@ class AddLinky extends React.Component {
 							className="input"
 							name="linky-value"
 							placeholder="Share a link"
-							onFocus={() => this.setState({ showButton: true })}
-							onBlur={() => this.setState({ showButton: false })}
+							onFocus={() => this.setState({ isFolded: false })}
+							onClick={() => this.setState({ isFolded: false })}
 						/>
 					</div>
 				</div>
-				<div className={`add-linky-actions ${this.state.showButton ? '' : 'hidden'}`}>
+				<div className={`field ${this.state.isFolded ? 'hidden' : ''}`}>
+					<div className="control">
+						<input
+							className="input is-small"
+							name="linky-tags"
+							placeholder="Set some tags"
+							size="40"
+						/>
+					</div>
+				</div>
+				<div className={`add-linky-actions ${this.state.isFolded ? 'hidden' : ''}`}>
 					<button className="button is-primary is-pulled-right add-linky">Share it!</button>
 				</div>
 			</section>
